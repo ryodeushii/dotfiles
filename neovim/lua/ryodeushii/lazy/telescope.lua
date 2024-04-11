5bc2df09f8a9ad590c33e9c285c853729ee1bcf2
Evgeny K <3193631@gmail.com>
2024-04-11 11:03:58 +0200

    feat: return builtin grep keybind

--- neovim/lua/ryodeushii/lazy/telescope.lua
+++ neovim/lua/ryodeushii/lazy/telescope.lua
@@ -29,7 +29,7 @@ return {
 
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fg', function()
+        vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
