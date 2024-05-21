fb34cdef18e8c17300296eee64be79445bcdcd37
Evgeny K <3193631@gmail.com>
2024-05-21 10:27:40 +0200

    feat: add copilot chat

--- neovim/lua/ryodeushii/lazy/copilot.lua
+++ neovim/lua/ryodeushii/lazy/copilot.lua
@@ -1,4 +1,4 @@
-return {
+return { {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
@@ -31,4 +31,20 @@ return {
             }
         })
     end,
+},
+    {
+        {
+            "CopilotC-Nvim/CopilotChat.nvim",
+            branch = "canary",
+            dependencies = {
+                { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+                { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+            },
+            opts = {
+                debug = true, -- Enable debugging
+                -- See Configuration section for rest
+            },
+            -- See Commands section for default commands if you want to lazy load on them
+        },
+    }
 }
