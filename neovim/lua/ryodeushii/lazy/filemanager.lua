40f2ce04689f18f404a2338f0dd82cd471b4fcc7
Evgeny K <3193631@gmail.com>
2024-06-07 14:19:25 +0200

    feat: show hidden in oil, use as default file manager, remove auto format for eslint

--- neovim/lua/ryodeushii/lazy/filemanager.lua
+++ neovim/lua/ryodeushii/lazy/filemanager.lua
@@ -2,7 +2,10 @@ return {
     "stevearc/oil.nvim",
     config = function()
         require("oil").setup({
-            default_file_explorer = false
+            default_file_explorer = true,
+            view_options = {
+                show_hidden = true,
+            }
         })
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
--- neovim/lua/ryodeushii/lazy/lsp.lua
+++ neovim/lua/ryodeushii/lazy/lsp.lua
@@ -139,17 +139,17 @@ return {
 
                 on_attach = function(client, bufnr)
                     client.server_capabilities.documentFormattingProvider = true
-                    if client.server_capabilities.documentFormattingProvider then
-                        local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            pattern = "*",
-                            callback = function()
-                                vim.lsp.buf.format({ async = true })
-                            end,
-                            group = au_lsp,
-
-                        })
-                    end
+                    -- if client.server_capabilities.documentFormattingProvider then
+                    --     local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
+                    --     vim.api.nvim_create_autocmd("BufWritePre", {
+                    --         pattern = "*",
+                    --         callback = function()
+                    --             vim.lsp.buf.format({ async = true })
+                    --         end,
+                    --         group = au_lsp,
+                    --
+                    --     })
+                    -- end
                 end,
 
             })
