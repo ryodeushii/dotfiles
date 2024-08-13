4f1a626c3c2c3c8a751ec5236059482b3ccbb375
Evgeny K <3193631@gmail.com>
2024-08-13 08:48:59 +0200

    feat: add cpp in copilot

--- neovim/lua/ryodeushii/lazy/copilot.lua
+++ neovim/lua/ryodeushii/lazy/copilot.lua
@@ -1,51 +1,52 @@
 return { {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
 
-    config = function()
-        require("copilot").setup({
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                debounce = 75,
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
 
-            },
-            filetypes = {
-                javascript = true,
-                typescript = true,
-                rust = true,
-                lua = true,
-                perl = true,
-                go = true,
-                yaml = true,
-                json = true,
-                toml = true,
-                python = true,
-                markdown = true,
-                vim = true,
-                sh = true,
-                bash = true,
-                groovy = true,
-                c = true,
-                ["*"] = false,
-            }
-        })
-    end,
+      },
+      filetypes = {
+        javascript = true,
+        typescript = true,
+        rust = true,
+        lua = true,
+        perl = true,
+        go = true,
+        yaml = true,
+        json = true,
+        toml = true,
+        python = true,
+        markdown = true,
+        vim = true,
+        sh = true,
+        bash = true,
+        groovy = true,
+        c = true,
+        cpp = true,
+        ["*"] = false,
+      }
+    })
+  end,
 },
+  {
     {
-        {
-            "CopilotC-Nvim/CopilotChat.nvim",
-            branch = "canary",
-            dependencies = {
-                { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-                { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-            },
-            opts = {
-                debug = true, -- Enable debugging
-                -- See Configuration section for rest
-            },
-            -- See Commands section for default commands if you want to lazy load on them
-        },
-    }
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      dependencies = {
+        { "zbirenbaum/copilot.lua" },         -- or github/copilot.vim
+        { "nvim-lua/plenary.nvim" },          -- for curl, log wrapper
+      },
+      opts = {
+        debug = true,         -- Enable debugging
+        -- See Configuration section for rest
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
+  }
 }
