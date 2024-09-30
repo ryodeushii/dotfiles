f411d5bb245c8e5514ce928fc673fa399cf14017
Evgeny K <3193631@gmail.com>
2024-09-30 12:57:31 +0200

    feat: update dashboard ui config

--- neovim/lua/ryodeushii/lazy/term.lua
+++ neovim/lua/ryodeushii/lazy/term.lua
@@ -4,9 +4,32 @@ return {
     { "willothy/flatten.nvim", config = true, priority = 1001, },
     {
       "goolord/alpha-nvim",
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
       config = function()
-        require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
-      end
+        local dashboard = require("alpha.themes.dashboard")
+        local buttons = {
+          type = "group",
+          val = {
+            { type = "text",    val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+            { type = "padding", val = 1 },
+            dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+            dashboard.button("SPC f f", "󰈞  Find file"),
+            dashboard.button("SPC f s", "󰊄  Live grep"),
+            dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+            dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+          },
+          position = "center",
+        }
+        local alpha_config = require("alpha.themes.theta")
+        -- available: devicons, mini, default is mini
+        -- if provider not loaded and enabled is true, it will try to use another provider
+        alpha_config.file_icons.provider = "devicons"
+        alpha_config.buttons = buttons
+        require("alpha").setup(
+          alpha_config.config
+        )
+      end,
+
     },
   },
   config = function()
