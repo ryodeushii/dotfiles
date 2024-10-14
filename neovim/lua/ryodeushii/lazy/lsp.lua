67ce1a0da84a47675c1165c112eac8db92e46e30
Evgeny K <3193631@gmail.com>
2024-10-14 12:21:16 +0200

    feat: add yamlls

--- neovim/lua/ryodeushii/lazy/lsp.lua
+++ neovim/lua/ryodeushii/lazy/lsp.lua
@@ -56,6 +56,7 @@ return {
           "clangd",
           "bashls",
           "ast_grep",
+          "yamlls",
           -- "prettierd",
           -- "prettier",
           -- "shfmt",
@@ -66,6 +67,36 @@ return {
               capabilities = capabilities
             }
           end,
+
+          ["yamlls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.yamlls.setup {
+              capabilities = capabilities,
+              settings = {
+                yaml = {
+                  format = {
+                    enable = true,
+                  },
+                  schemas = {
+                    kubernetes = "*.yaml",
+                    ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                    ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                    ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                    ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                    ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                    ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                    ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                    ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                    ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-{compose,stack}*.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+                  }
+                }
+              }
+            }
+          end,
+
 
           ["lua_ls"] = function()
             local lspconfig = require("lspconfig")
