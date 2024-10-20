b9475203725dc636b6f57841e8c64ee11c28b338
Evgeny K <3193631@gmail.com>
2024-10-21 01:06:53 +0200

    feat(harpoon): quick switch remap to A-(1,2,3,4)

--- neovim/lua/ryodeushii/lazy/harpoon.lua
+++ neovim/lua/ryodeushii/lazy/harpoon.lua
@@ -17,10 +17,10 @@ return {
     -- add file to list
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     -- quick switch between first 4 files
-    vim.keymap.set("n", "<C-f>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-b>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-y>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
 
     -- show ui (internal)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
