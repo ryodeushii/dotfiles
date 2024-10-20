5d2f311ca81e0e5e7d7d4197dd566cf90b2f48bd
Evgeny K <3193631@gmail.com>
2024-10-21 00:09:15 +0200

    fix: dependabot v2 schema, js snippets

--- neovim/lua/ryodeushii/lazy/lsp.lua
+++ neovim/lua/ryodeushii/lazy/lsp.lua
@@ -86,7 +86,7 @@ return {
                     ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
                     ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = "*play*.{yml,yaml}",
                     ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                    ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                    ["https://json.schemastore.org/dependabot-2.0"] = ".github/dependabot.{yml,yaml}",
                     ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
                     ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
                     ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-{compose,stack}*.{yml,yaml}",
