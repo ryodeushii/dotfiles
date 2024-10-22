0a13e47330b9378a5eafab6640c35920d1ffd5dd
Evgeny K <3193631@gmail.com>
2024-10-22 17:17:22 +0200

    fix: filename in lualine

--- neovim/lua/ryodeushii/lazy/statusline.lua
+++ neovim/lua/ryodeushii/lazy/statusline.lua
@@ -1,50 +1,54 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require('lualine').setup({
-            options = {
-                icons_enabled = true,
-                theme = 'catppuccin',
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {},
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require('lualine').setup({
+      options = {
+        icons_enabled = true,
+        theme = 'catppuccin',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
 
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                globalstatus = false,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                }
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'filename' },
-                lualine_c = { 'branch', 'diff', 'diagnostics' },
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          { 'filename', path = 1 },
+        },
+        lualine_c = { 'branch', 'diff', 'diagnostics' },
 
-                lualine_x = { 'filetype' },
-                lualine_y = {},
-                lualine_z = { 'location' }
-            },
-            inactive_sections = {
-                lualine_a = { 'filename' },
-                lualine_b = {},
-                lualine_c = {},
+        lualine_x = { 'filetype', 'fileformat' },
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {
+          { 'filename', path = 0 },
+        },
+        lualine_b = {},
+        lualine_c = {},
 
-                lualine_x = { 'filetype', 'location' },
-                lualine_y = {},
-                lualine_z = {}
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
-        })
-    end
+        lualine_x = { 'filetype', 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+    })
+  end
 }
