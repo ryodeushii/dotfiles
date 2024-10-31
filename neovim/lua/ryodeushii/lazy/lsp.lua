390e28e7756415862eefaadc910668c1916ba585
Evgeny K <3193631@gmail.com>
2024-10-31 19:25:21 +0100

    feat: rework lua_ls to support neovim typings

--- neovim/lua/ryodeushii/lazy/lsp.lua
+++ neovim/lua/ryodeushii/lazy/lsp.lua
@@ -110,7 +110,7 @@ return {
                     kubernetes = "*.yaml",
                     ["http://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
                     ["https://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "roles/tasks/*.{yml,yaml}",
+                    -- ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "roles/tasks/*.{yml,yaml}",
                     ["https://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
                     ["http://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
                     ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = "*play*.{yml,yaml}",
@@ -131,6 +131,33 @@ return {
             local lspconfig = require("lspconfig")
 
             lspconfig.lua_ls.setup {
+              on_init = function(client)
+                if client.workspace_folders then
+                  local path = client.workspace_folders[1].name
+                  if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+                    return
+                  end
+                end
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                  runtime = {
+                    -- Tell the language server which version of Lua you're using
+                    -- (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT'
+                  },
+                  -- Make the server aware of Neovim runtime files
+                  workspace = {
+                    checkThirdParty = false,
+                    library = {
+                      vim.env.VIMRUNTIME
+                      -- Depending on the usage, you might want to add additional paths here.
+                      -- "${3rd}/luv/library"
+                      -- "${3rd}/busted/library",
+                    }
+                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                    -- library = vim.api.nvim_get_runtime_file("", true)
+                  }
+                })
+              end,
               capabilities = capabilities,
               settings = {
                 Lua = {
