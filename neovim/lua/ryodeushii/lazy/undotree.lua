c4851bca2f2f72a1c9f2e5de4d51df15952aee7a
Evgeny K <3193631@gmail.com>
2024-11-12 15:28:26 +0100

    feat: add highlights for undo, redo and p[P]aste

--- neovim/lua/ryodeushii/lazy/undotree.lua
+++ neovim/lua/ryodeushii/lazy/undotree.lua
@@ -1,7 +1,17 @@
 return {
-  "mbbill/undotree",
+  {
+    "tzachar/highlight-undo.nvim",
+    config = function()
+      require('highlight-undo').setup({
+        duration = 300,
+      })
+    end,
+  },
+  {
+    "mbbill/undotree",
 
-  config = function()
-    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-  end
+    config = function()
+      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    end
+  }
 }
