31d1018a026e46dd39e5338ecb0d219b0340974f
Evgeny K <3193631@gmail.com>
2024-12-01 19:08:45 +0100

    feat: enable <leader>. for scratch file

--- neovim/lua/ryodeushii/lazy/snacks.lua
+++ neovim/lua/ryodeushii/lazy/snacks.lua
@@ -16,19 +16,22 @@ return {
           "nvim-lua/plenary.nvim",
         },
         keys = {
-        { "<leader>gs", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+          { "<leader>gs", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
 
       }
     },
     priority = 1000,
     lazy = false,
+    keys = {
+      { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    },
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
       bigfile = { enabled = true },
-      dashboard = {enabled = false},
+      dashboard = { enabled = false },
       notifier = { enabled = true },
       quickfile = { enabled = true },
       statuscolumn = { enabled = false },
