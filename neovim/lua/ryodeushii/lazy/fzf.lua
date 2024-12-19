f186f553bba08bf59d8b3b5ea132b3ae06a361b2
Evgeny K <3193631@gmail.com>
2024-12-19 12:05:45 +0100

    feat: attempt to replace telescope with fzf-lua

---
+++ neovim/lua/ryodeushii/lazy/fzf.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local fzf = require("fzf-lua")
+      -- local trouble = require("trouble.sources.fzf")
+
+      local config = require("fzf-lua.config")
+      local actions = require("trouble.sources.fzf").actions
+      config.defaults.actions.files["ctrl-t"] = actions.open
+      config.defaults.actions.files["ctrl-q"] = actions.file_edit_or_qf
+
+      vim.keymap.set('n', '<leader>ff', fzf.files, { desc = 'Find files' })
+      vim.keymap.set('n', '<leader>ft', fzf.treesitter, { desc = 'Find treesitter symbols in current buffer' })
+      -- https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#commands
+      vim.keymap.set('n', '<leader>fo', fzf.oldfiles, { desc = 'Find old files' })
+      vim.keymap.set('n', '<C-p>', fzf.files, { desc = 'Find files' })
+      vim.keymap.set('n', '<leader>fb', fzf.buffers, { desc = 'Find buffers' })
+      vim.keymap.set("n", "<leader>fr", fzf.resume, { desc = "Resume last window" })
+      vim.keymap.set("n", "<leader>fw", fzf.grep_cword, { desc = "Find current word" })
+      vim.keymap.set("v", "<leader>fw", fzf.grep_visual, { desc = "Find current word" })
+      vim.keymap.set("n", "<leader>ps", fzf.lgrep_curbuf, { desc = "Live grep current buffer" })
+      vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "Find keymaps" })
+      vim.keymap.set("n", "<leader>fs", fzf.live_grep, { desc = "Find text in project" })
+      vim.keymap.set("n", "<leader>fh", fzf.helptags, { desc = "Find help tags" })
+    end
+  }
+}
--- neovim/lua/ryodeushii/lazy/telescope.lua
+++ neovim/lua/ryodeushii/lazy/telescope.lua
@@ -36,25 +36,5 @@ return {
         }
       }
     })
-
-    local builtin = require('telescope.builtin')
-
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set("n", "<leader>fs", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fr', ":Telescope resume<CR>")
-    -- vim.keymap.set('n', '<leader>pws', function()
-    --     local word = vim.fn.expand("<cword>")
-    --     builtin.grep_string({ search = word })
-    -- end)
-    -- vim.keymap.set('n', '<leader>pWs', function()
-    --     local word = vim.fn.expand("<cWORD>")
-    --     builtin.grep_string({ search = word })
-    -- end)
   end
 }
