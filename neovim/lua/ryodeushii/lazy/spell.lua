67bfc7b2c9edfd350f0c7cee16e66996c5e42bda
Evgeny Kapusta <kpae@viessmann.com>
2024-12-24 10:46:45 +0100

    fix: wrong command for spell suggest

--- neovim/lua/ryodeushii/lazy/spell.lua
+++ neovim/lua/ryodeushii/lazy/spell.lua
@@ -1,15 +1,15 @@
-return (
+return {
   {
     "ravibrock/spellwarn.nvim",
     event = "VeryLazy",
     config = function()
       require("spellwarn").setup()
-      -- use Alt + a or alt + A to add a word to the spellfile
+      -- use Alt + a or alt + A to add a word to the spell file
       vim.keymap.set('n', '<A-a>', "zg", { silent = true, desc = "Add word to allowed spell list" })
       -- -- use Alt + s to suggest a word
-      vim.keymap.set('n', '<leader><leader>s', "Telescope spell_suggest<CR>", { silent = true, desc = "Suggest words with telescope" })
+      vim.keymap.set('n', '<A-s>', ":Telescope spell_suggest<CR>", { silent = true, desc = "Suggest words with telescope" })
       -- -- use Alt + S to toggle spell checking
       vim.keymap.set('n', '<A-S>', ":setlocal spell!<CR>", { silent = true, desc = "Toggle spell checking" })
     end,
   }
-)
+}
