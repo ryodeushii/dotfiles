3a00092462655fc2ef5f443309d2fb5ea81073f9
Evgeny Kapusta <kpae@viessmann.com>
2024-12-24 10:43:59 +0100

    fix: spell suggest keymap, deps, dict

--- neovim/lua/ryodeushii/lazy/spell.lua
+++ neovim/lua/ryodeushii/lazy/spell.lua
@@ -1,4 +1,4 @@
-return {
+return (
   {
     "ravibrock/spellwarn.nvim",
     event = "VeryLazy",
@@ -7,9 +7,9 @@ return {
       -- use Alt + a or alt + A to add a word to the spellfile
       vim.keymap.set('n', '<A-a>', "zg", { silent = true, desc = "Add word to allowed spell list" })
       -- -- use Alt + s to suggest a word
-      vim.keymap.set('n', '<A-s>', "Telescope spell_suggest<CR>", { silent = true, desc = "Suggest words with telescope" })
+      vim.keymap.set('n', '<leader><leader>s', "Telescope spell_suggest<CR>", { silent = true, desc = "Suggest words with telescope" })
       -- -- use Alt + S to toggle spell checking
       vim.keymap.set('n', '<A-S>', ":setlocal spell!<CR>", { silent = true, desc = "Toggle spell checking" })
     end,
   }
-}
+)
