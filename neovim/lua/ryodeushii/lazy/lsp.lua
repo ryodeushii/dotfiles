fbb70785456bf55bb91dc44708b4b3a453f958be
Evgeny Kapusta <kpae@viessmann.com>
2025-01-03 11:42:43 +0100

    feat(completion):  ✨ adjust configuration, source order, update deps

--- neovim/lua/ryodeushii/lazy/lsp.lua
+++ neovim/lua/ryodeushii/lazy/lsp.lua
@@ -1,32 +1,146 @@
 return {
   {
-    'saghen/blink.cmp',
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = '*',
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
     lazy = false, -- lazy loading handled internally
     -- optional: provides snippets for the snippet source
     dependencies = {
       {
-        'L3MON4D3/LuaSnip',
+        "L3MON4D3/LuaSnip",
         version = 'v2.*',
-      }
+      },
+      {
+        "David-Kunz/cmp-npm",
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        ft = "json",
+        config = function()
+          require('cmp-npm').setup({
+            only_latest_version = false,
+            only_semantic_versions = true,
+          })
+        end
+      },
+      {
+        "Snikimonkd/cmp-go-pkgs",
+      },
     },
-    version = '*',
+    version = "*",
     opts_extend = { "sources.completion.enabled_providers" },
-    opts = {
-      snippets = {
+    opts = function(_, opts)
+      opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
+        default = { "lsp", "path", "snippets", "buffer", "luasnip", "npm", "go_pkgs" },
+        -- default = { "lsp", "luasnip", "path", "buffer", "npm", "go_pkgs" },
+        providers = {
+          lsp = {
+            name = "lsp",
+            enabled = true,
+            module = "blink.cmp.sources.lsp",
+            -- When linking markdown notes, I would get snippets and text in the
+            -- suggestions, I want those to show only if there are no LSP
+            -- suggestions
+            -- Disabling fallbacks as my snippets woudlnt show up
+            -- fallbacks = { "luasnip", "buffer" },
+            score_offset = 90, -- the higher the number, the higher the priority
+          },
+          luasnip = {
+            name = "luasnip",
+            enabled = true,
+            module = "blink.cmp.sources.luasnip",
+            min_keyword_length = 2,
+            fallbacks = { "snippets" },
+            score_offset = 85,
+            max_items = 8,
+            -- Only show luasnip items if I type the trigger_text characters, so
+            -- to expand the "bash" snippet, if the trigger_text is ";" I have to
+            -- type ";bash"
+            -- After accepting the completion, delete the trigger_text characters
+            -- from the final inserted text
+          },
+          path = {
+            name = "Path",
+            module = "blink.cmp.sources.path",
+            score_offset = 3,
+            -- When typing a path, I would get snippets and text in the
+            -- suggestions, I want those to show only if there are no path
+            -- suggestions
+            fallbacks = { "luasnip", "buffer" },
+            opts = {
+              trailing_slash = false,
+              label_trailing_slash = true,
+              get_cwd = function(context)
+                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+              end,
+              show_hidden_files_by_default = true,
+            },
+          },
+          buffer = {
+            name = "Buffer",
+            enabled = true,
+            max_items = 3,
+            module = "blink.cmp.sources.buffer",
+            min_keyword_length = 4,
+          },
+          snippets = {
+            name = "snippets",
+            enabled = true,
+            max_items = 3,
+            module = "blink.cmp.sources.snippets",
+            min_keyword_length = 4,
+            score_offset = 80, -- the higher the number, the higher the priority
+          },
+
+          npm = {
+            name = "npm", -- IMPORTANT: use the same name as you would for nvim-cmp
+            module = "blink.compat.source",
+            opts = {
+              keyword_length = 4
+            },
+          },
+          go_pkgs = {
+            name = "go_pkgs",
+            module = "blink.compat.source",
+            opts = {
+              disallow_symbol_nonprefix_matching = false
+            }
+          }
+        },
+        -- command line completion, thanks to dpetka2001 in reddit
+        -- https://www.reddit.com/r/neovim/comments/1hjjf21/comment/m37fe4d/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+      })
+
+      opts.snippets = {
         expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
         active = function(filter)
           if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
+            return require("luasnip").jumpable(filter.direction)
           end
-          return require('luasnip').in_snippet()
+          return require("luasnip").in_snippet()
         end,
-        jump = function(direction) require('luasnip').jump(direction) end,
-      },
-      appearance = {
+        jump = function(direction) require("luasnip").jump(direction) end,
+      }
+      opts.appearance = {
         use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono'
-      },
-      completion = {
+        nerd_font_variant = "mono"
+      }
+      opts.completion = {
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
         menu = {
           draw = {
@@ -36,18 +150,29 @@ return {
             },
           },
         },
-      },
-      sources = {
-        cmdline = {},
-        default = { "lsp", "luasnip", "path", "buffer" },
-      },
-      keymap = {
+      }
+
+      opts.keymap = {
         preset = "default",
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-      },
-      signature = { enabled = true }
-    },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+      }
+
+      opts.signature = { enabled = true }
+
+      return opts
+    end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
