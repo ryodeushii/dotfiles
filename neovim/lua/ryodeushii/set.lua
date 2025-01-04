ae7a23e9975af7c922885a804e6d737734de0de0
Evgeny K <3193631@gmail.com>
2025-01-04 02:22:29 +0100

    fix(clipboard):  🐛 allow vim autodetect, performance loss on wsl not worth it

--- neovim/lua/ryodeushii/set.lua
+++ neovim/lua/ryodeushii/set.lua
@@ -42,20 +42,20 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevelstart = 99
 
-if vim.fn.has('wsl') == 1 then
-    vim.g.clipboard = {
-        name = 'WslClipboard',
-        copy = {
-            ['+'] = 'clip.exe',
-            ['*'] = 'clip.exe',
-        },
-        paste = {
-            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
-    }
-end
+-- if vim.fn.has('wsl') == 1 then
+--     vim.g.clipboard = {
+--         name = 'WslClipboard',
+--         copy = {
+--             ['+'] = 'clip.exe',
+--             ['*'] = 'clip.exe',
+--         },
+--         paste = {
+--             ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--             ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--         },
+--         cache_enabled = 0,
+--     }
+-- end
 
 vim.opt.clipboard:append("unnamedplus")
 
