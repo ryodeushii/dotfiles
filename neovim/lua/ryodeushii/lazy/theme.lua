7a83f9880147c09fd2df2e16232bc378148d70b8
Evgeny K <3193631@gmail.com>
2025-01-05 18:40:51 +0100

    feat: switch backto rose-pine, update deps

--- neovim/lua/ryodeushii/lazy/theme.lua
+++ neovim/lua/ryodeushii/lazy/theme.lua
@@ -1,6 +1,6 @@
 -- call this function to apply a color scheme in any module
 function ApplyColorScheme(color)
-  color = color or "tokyodark"
+  color = color or "rose-pine"
 
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -10,24 +10,29 @@ end
 
 return {
   {
-    "tiagovla/tokyodark.nvim",
+    "rose-pine/neovim",
     priority = 999,
+    lazy = false,
+    name = "rose-pine",
     config = function()
-      require("tokyodark").setup({
-        transparent_background = true,
-        gamma = 1.05,                                                          -- adjust the brightness of the theme
-        styles = {
-          comments = { italic = false },                                       -- style for comments
-          keywords = { italic = false },                                       -- style for keywords
-          identifiers = { italic = false },                                    -- style for identifiers
-          functions = {},                                                      -- style for functions
-          variables = {},                                                      -- style for variables
+      require('rose-pine').setup({
+        highlight_groups = {
+          Normal = { bg = "#061111" },
+          NormalNC = { bg = "#061111" },
         },
-        custom_highlights = {} or function(highlights, palette) return {} end, -- extend highlights
-        custom_palette = {} or function(palette) return {} end,                -- extend palette
-        terminal_colors = true,                                                -- enable terminal colors
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+        variant = "moon",
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+        enable = {
+          terminal = true,
+        },
       })
+
       ApplyColorScheme()
-    end,
-  },
+    end
+  }
 }
