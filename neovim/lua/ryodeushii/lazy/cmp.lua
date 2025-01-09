1224a48257db9355c7d45af8e6b6d6fb87e88971
Evgeny Kapusta <kpae@viessmann.com>
2025-01-09 13:09:56 +0100

    fix: after blink.cmp update

--- neovim/lua/ryodeushii/lazy/cmp.lua
+++ neovim/lua/ryodeushii/lazy/cmp.lua
@@ -123,7 +123,7 @@ return {
     --- @param opts blink.cmp.Config
     opts = function(_, opts)
       opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-        default = { "lsp", "path", "snippets", "buffer", "luasnip", "npm", "markdown" },
+        default = { "lsp", "path", "snippets", "buffer", "npm", "markdown" },
         providers = {
           markdown = {
             name = 'RenderMarkdown',
@@ -136,20 +136,11 @@ return {
             module = "blink.cmp.sources.lsp",
             score_offset = 90, -- the higher the number, the higher the priority
           },
-          luasnip = {
-            name = "luasnip",
-            enabled = true,
-            module = "blink.cmp.sources.luasnip",
-            min_keyword_length = 2,
-            fallbacks = { "snippets" },
-            score_offset = 85,
-            max_items = 8,
-          },
           path = {
             name = "Path",
             module = "blink.cmp.sources.path",
             score_offset = 3,
-            fallbacks = { "luasnip", "buffer" },
+            fallbacks = { "snippets", "buffer" },
             opts = {
               trailing_slash = false,
               label_trailing_slash = true,
@@ -204,6 +195,7 @@ return {
       })
 
       opts.snippets = vim.tbl_deep_extend("force", opts.snippets or {}, {
+        preset = "luasnip",
         expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
         active = function(filter)
           if filter and filter.direction then
