d61d5c148df3893b4d1e5eff86df09d5d63d8755
Evgeny K <3193631@gmail.com>
2025-02-01 21:59:11 +0100

    feat: add vague theme and use it by default

--- neovim/lua/ryodeushii/lazy/theme.lua
+++ neovim/lua/ryodeushii/lazy/theme.lua
@@ -3,11 +3,13 @@ function ApplyColorScheme(color)
   color = color or "rose-pine-moon"
 
   vim.cmd.colorscheme(color)
-  local normal = vim.api.nvim_get_hl(0, { name = "NormalNC" })
-  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = normal.bg, fg = normal.fg })
-  vim.api.nvim_set_hl(0, "Normal", { bg = normal.bg, fg = normal.fg })
-  vim.api.nvim_set_hl(0, "FloatBorder", { bg = normal.bg, fg = normal.fg })
+
+  if color == "rose-pine-moon" then
+    local normal = vim.api.nvim_get_hl(0, { name = "NormalNC" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = normal.bg, fg = normal.fg })
+    vim.api.nvim_set_hl(0, "Normal", { bg = normal.bg, fg = normal.fg })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = normal.bg, fg = normal.fg })
+  end
 end
 
 return {
@@ -108,7 +110,94 @@ return {
         },
       })
 
-      ApplyColorScheme()
+      -- ApplyColorScheme()
+    end,
+  },
+  {
+    "vague2k/vague.nvim",
+    priority = 999,
+    lazy = false,
+    config = function()
+      require("vague").setup({
+        transparent = false, -- don't set background
+        style = {
+          -- "none" is the same thing as default. But "italic" and "bold" are also valid options
+          boolean = "bold",
+          number = "none",
+          float = "none",
+          error = "bold",
+          comments = "italic",
+          conditionals = "none",
+          functions = "none",
+          headings = "bold",
+          operators = "none",
+          strings = "italic",
+          variables = "none",
+
+          -- keywords
+          keywords = "none",
+          keyword_return = "italic",
+          keywords_loop = "none",
+          keywords_label = "none",
+          keywords_exception = "none",
+
+          -- builtin
+          builtin_constants = "bold",
+          builtin_functions = "none",
+          builtin_types = "bold",
+          builtin_variables = "none",
+        },
+        -- plugin styles where applicable
+        -- make an issue/pr if you'd like to see more styling options!
+        plugins = {
+          cmp = {
+            match = "bold",
+            match_fuzzy = "bold",
+          },
+          dashboard = {
+            footer = "italic",
+          },
+          lsp = {
+            diagnostic_error = "bold",
+            diagnostic_hint = "none",
+            diagnostic_info = "italic",
+            diagnostic_warn = "bold",
+          },
+          neotest = {
+            focused = "bold",
+            adapter_name = "bold",
+          },
+          telescope = {
+            match = "bold",
+          },
+        },
+        -- Override colors
+        colors = {
+          bg = "#141415",
+          fg = "#cdcdcd",
+          floatBorder = "#878787",
+          line = "#252530",
+          comment = "#606079",
+          builtin = "#b4d4cf",
+          func = "#c48282",
+          string = "#e8b589",
+          number = "#e0a363",
+          property = "#c3c3d5",
+          constant = "#aeaed1",
+          parameter = "#bb9dbd",
+          visual = "#333738",
+          error = "#df6882",
+          warning = "#f3be7c",
+          hint = "#7e98e8",
+          operator = "#90a0b5",
+          keyword = "#6e94b2",
+          type = "#9bb4bc",
+          search = "#405065",
+          plus = "#8cb66d",
+          delta = "#f3be7c",
+        },
+      })
+      ApplyColorScheme("vague")
     end,
   },
   -- {
