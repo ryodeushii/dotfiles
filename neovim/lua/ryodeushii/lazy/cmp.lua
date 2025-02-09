53614c39bdf4fb915e19e94841d451ba03aff9a0
Evgeny K <3193631@gmail.com>
2025-02-09 22:13:35 +0100

    feat: get rid of colorful menu in cmp

--- neovim/lua/ryodeushii/lazy/cmp.lua
+++ neovim/lua/ryodeushii/lazy/cmp.lua
@@ -1,6 +1,6 @@
 --- @module 'blink.cmp'
 
--- TODO: write custopm cmp source for blink.cmp to complete available versions while in go.mod file
+-- TODO: write custom cmp source for blink.cmp to complete available versions while in go.mod file
 -- go list -m -versions -json github.com/go-playground/validator/v10
 -- golang commat to find versions of a module (to use in custom cmp source for go mod)
 
@@ -46,51 +46,6 @@ return {
     version = "*",
     lazy = true,
     opts = {},
-  },
-  {
-    "xzbdmw/colorful-menu.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("colorful-menu").setup({
-        ls = {
-          lua_ls = {
-            -- Maybe you want to dim arguments a bit.
-            arguments_hl = "@comment",
-          },
-          gopls = {
-            -- When true, label for field and variable will format like "foo: Foo"
-            -- instead of go's original syntax "foo Foo".
-            add_colon_before_type = false,
-          },
-          ["typescript-language-server"] = {
-            extra_info_hl = "@comment",
-          },
-          ts_ls = {
-            extra_info_hl = "@comment",
-          },
-          vtsls = {
-            extra_info_hl = "@comment",
-          },
-          ["rust-analyzer"] = {
-            -- Such as (as Iterator), (use std::io).
-            extra_info_hl = "@comment",
-          },
-          clangd = {
-            -- Such as "From <stdio.h>".
-            extra_info_hl = "@comment",
-          },
-          -- If true, try to highlight "not supported" languages.
-          fallback = true,
-        },
-        -- If the built-in logic fails to find a suitable highlight group,
-        -- this highlight is applied to the label.
-        fallback_highlight = "@variable",
-        -- If provided, the plugin truncates the final displayed text to
-        -- this width (measured in display cells). Any highlights that extend
-        -- beyond the truncation point are ignored. Default 60.
-        max_width = 60,
-      })
-    end,
   },
   {
     "saghen/blink.cmp",
@@ -242,25 +197,6 @@ return {
             components = {
               label = {
                 width = { fill = true, max = 60 },
-                text = function(ctx)
-                  local highlights_info = require("colorful-menu").blink_highlights(ctx)
-                  if highlights_info ~= nil then
-                    return highlights_info.label
-                  else
-                    return ctx.label
-                  end
-                end,
-                highlight = function(ctx)
-                  local highlights = {}
-                  local highlights_info = require("colorful-menu").blink_highlights(ctx)
-                  if highlights_info ~= nil then
-                    highlights = highlights_info.highlights
-                  end
-                  for _, idx in ipairs(ctx.label_matched_indices) do
-                    table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-                  end
-                  return highlights
-                end,
               },
             },
           },
