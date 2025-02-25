311c0845c88b5d59a745afecb59a34743cb245d7
Evgeny K <3193631@gmail.com>
2025-02-25 18:11:24 +0100

    feat: temporarily enable copilot and copilot chat

--- neovim/lua/ryodeushii/lazy/ai.lua
+++ neovim/lua/ryodeushii/lazy/ai.lua
@@ -1,5 +1,75 @@
 return {
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<A-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<A-]>",
+            prev = "<A-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          snacks_picker_input = false,
+          snacks_input = false,
+          oil = false,
+          javascript = true,
+          typescript = true,
+          rust = true,
+          lua = true,
+          perl = true,
+          go = true,
+          yaml = true,
+          json = true,
+          toml = true,
+          python = true,
+          markdown = true,
+          vim = true,
+          sh = true,
+          bash = true,
+          groovy = true,
+          c = true,
+          cpp = true,
+          ["*"] = false,
+        },
+      })
+    end,
+  },
+  {
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      cmd = "CopilotChat",
+      branch = "main",
+      dependencies = {
+        { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      },
+      opts = {
+        debug = false, -- Enable debugging
+        -- See Configuration section for rest
+      },
+      keys = {
+        {
+          "<leader>ch",
+          "<cmd>CopilotChat<CR>",
+          silent = true,
+          desc="Open Copilot Chat",
+          mode = {"n", "v"},
+        }
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
+  },
+  --[[ {
     "Exafunction/codeium.nvim",
     event = "InsertEnter",
     cmd = "Codeium",
@@ -9,6 +79,7 @@ return {
     version = "*",
     config = function()
       require("codeium").setup({
+        enabled = false,
         enable_cmp_source = false,
         virtual_text = {
           enabled = true,
@@ -31,5 +102,5 @@ return {
         },
       })
     end,
-  },
+  }, ]]
 }
