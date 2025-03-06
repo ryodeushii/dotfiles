3ef8b72061bfdc0ec4dfcfa33ef9fbaa02c8ad4f
Evgeny K <3193631@gmail.com>
2025-03-06 22:30:18 +0100

    feat: change comment block to gC from gb

--- neovim/lua/ryodeushii/lazy/comment.lua
+++ neovim/lua/ryodeushii/lazy/comment.lua
@@ -2,7 +2,20 @@ return {
   {
     "numToStr/Comment.nvim",
     config = function()
-      require("Comment").setup()
+      require("Comment").setup({
+        opleader = {
+          line = "gc",
+          block = "gC",
+        },
+        extra = {
+          ---Add comment on the line above
+          above = "gcO",
+          ---Add comment on the line below
+          below = "gco",
+          ---Add comment at the end of line
+          eol = "gcA",
+        },
+      })
     end,
   },
 }
