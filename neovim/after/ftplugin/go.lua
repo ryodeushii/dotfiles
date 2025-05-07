890d32673319f29e80bd7f0c97c9e194a0aa9f73
Evgeny K <3193631@gmail.com>
2025-05-07 14:39:07 +0200

    chore: remove comment from go.lua

--- neovim/after/ftplugin/go.lua
+++ neovim/after/ftplugin/go.lua
@@ -20,29 +20,3 @@ vim.keymap.set(
   "<cmd>GolangciLintFix<CR>",
   { silent = true, desc = "Run golangci-lint with fix" }
 )
-
--- NOTE: for now - unused, wait for golangci-lint v2 to work correctly with workspaces
--- when lsp format used in go, it should call lsp format commadn and then call GolangciLintFix
--- vim.keymap.del("n", "<leader>f")
--- vim.keymap.set(
---   "n",
---   "<leader>f",
---   function()
---     vim.lsp.buf.format()
---     -- vim.cmd("GolangciLintFix")
---     local cmd = "golangci-lint run --fix"
---     if vim.fn.filereadable("go.work") then
---       cmd = "go work edit -json | jq -r '.Use[].DiskPath'  | xargs -I{} golangci-lint run --fix {}/..."
---     end
---     local id = "golangci-lint-fix"
---     vim.notify("Running...", vim.log.levels.INFO, { title = "GolangciLintFix", timeout = 1000, id = id })
---     vim.fn.jobstart(cmd, {
---       on_exit = function(_, code)
---         local level = code == 0 and vim.log.levels.INFO or vim.log.levels.ERROR
---         vim.notify('Done', level, { title = "GolangciLintFix", timeout = 1000, id = id })
---         vim.cmd("e!")
---       end,
---     })
---   end,
---   { desc = "[go] Format buffer and golangci-lint-fix", silent = true }
--- )
